eb851e3f09e79e8611f0616773ecfe7c2238a29c
LuciferIsDead <shahidul.sumon69@gmail.com>
2026-07-11 12:22:25 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const startBtn = document.getElementById("startBtn");
 
+startBtn.addEventListener("click", () => {
+
+    document.body.style.transition = "1s";
+    document.body.style.opacity = "0";
+
+    setTimeout(() => {
+        alert("✨ Welcome to our journey...\n\nVersion 1.0 Complete ❤️");
+
+        document.body.style.opacity = "1";
+    },1000);
+
+});
