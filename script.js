d02bbc193db34b4a917d49c30354ba37fb781c0f
LuciferIsDead <shahidul.sumon69@gmail.com>
2026-07-12 23:31:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-const startBtn = document.getElementById("startBtn");
+const reliveBtn = document.getElementById("reliveBtn");
 
-startBtn.addEventListener("click", () => {
+const homeScreen = document.getElementById("homeScreen");
+const storyScreen = document.getElementById("storyScreen");
 
-    document.body.style.transition = "1s";
-    document.body.style.opacity = "0";
+const chapterTitle = document.getElementById("chapterTitle");
+const chapterText = document.getElementById("chapterText");
+
+const nextStory = document.getElementById("nextStory");
+
+// Start Chapter 1
+reliveBtn.addEventListener("click", () => {
+
+    homeScreen.classList.add("fadeOut");
 
     setTimeout(() => {
-        alert("✨ Welcome to our journey...\n\nVersion 1.0 Complete ❤️");
 
-        document.body.style.opacity = "1";
-    },1000);
+        homeScreen.style.display = "none";
+
+        storyScreen.classList.remove("hidden");
+
+        storyScreen.classList.add("fadeIn");
+
+        chapterTitle.textContent = "The Night Everything Changed";
+
+        chapterText.innerHTML =
+        `It all started with one little reply.<br><br>
+        "Ghumai."<br><br>
+        "Ghuman."<br><br>
+        Neither of us knew that this tiny conversation
+        would become the beginning of our favourite story.`;
+
+    },800);
+
+});
+
+// Next button (temporary)
+nextStory.addEventListener("click", () => {
+
+    alert("Chapter 2 will be added in Version 1.2 ❤️");
 
 });
