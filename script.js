2d578dffb08de5bd69fe6be30842872673e5aa52
LuciferIsDead <shahidul.sumon69@gmail.com>
2026-07-12 23:54:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,11 @@
-alert("SCRIPT VERSION 2");
+document.addEventListener("DOMContentLoaded", () => {
 
-// ==========================
-// ELEMENTS
-// ==========================
+    alert("NEW SCRIPT LOADED");
 
-const reliveBtn = document.getElementById("reliveBtn");
+    const btn = document.getElementById("reliveBtn");
 
-const homeScreen = document.getElementById("homeScreen");
-const storyScreen = document.getElementById("storyScreen");
-
-const chapterTitle = document.getElementById("chapterTitle");
-const chapterText = document.getElementById("chapterText");
-
-const nextStory = document.getElementById("nextStory");
-
-// ==========================
-// OPEN STORY
-// ==========================
-
-reliveBtn.addEventListener("click", openChapterOne);
-
-function openChapterOne() {
-
-    homeScreen.classList.add("fadeOut");
-
-    setTimeout(() => {
-
-        homeScreen.style.display = "none";
-
-        storyScreen.classList.remove("hidden");
-
-        storyScreen.style.display = "flex";
-
-        storyScreen.classList.add("fadeIn");
-
-        chapterTitle.innerHTML = "The Night Everything Changed";
-
-        chapterText.innerHTML = `
-        It all began with one tiny Instagram Note...<br><br>
-
-        <b>💭 Ghumai.</b><br><br>
-
-        <b>💙 Ghuman.</b><br><br>
-
-        Neither of us knew that one simple reply
-        would slowly become the most beautiful
-        chapter of our lives.
-        `;
-
-    }, 800);
-
-}
-
-// ==========================
-// NEXT BUTTON (Temporary)
-// ==========================
-
-nextStory.addEventListener("click", function () {
-
-    chapterTitle.innerHTML = "Coming Soon ❤️";
-
-    chapterText.innerHTML = `
-    Chapter 2 will continue our story.<br><br>
-
-    The Instagram Group.<br>
-    Samuel.<br>
-    Acting as a Couple.<br><br>
-
-    To be continued...
-    `;
+    btn.addEventListener("click", () => {
+        alert("NEW BUTTON");
+    });
 
 });
